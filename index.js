ba9f7dc30ecadcbac78cfdd0645953e3ac30acc9
Ebenezer <eaanertey@st.ug.edu.gh>
2021-10-25 07:59:57 +0000

    make it better

---
+++ index.js
@@ -0,0 +1,50 @@
+const {ApolloServer, gql} = require('apollo-server');
+
+
+
+const books =[
+    {
+        title: 'my First Copy book',
+        Author:'Ebenezer Abbey',
+        ISBN: '0-234-6784'
+    },
+    {
+        title:'Jurassic Park',
+        Author: 'kdueu kldjoij',
+    }
+]
+
+//Schemas
+const typeDefs= gql`
+type Book{
+    title: String!
+    Author: String!
+
+}
+type Query{
+    books:[Book]
+    
+}
+# type Mutation{}
+
+# type Subscription{}
+
+
+
+`;
+
+const resolvers = {
+    Query: {
+        books:()=>books,
+        // book: tit => tit===books.title? books.title : null
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+server.listen(2000).then(()=> console.log('running'))
+
+
+//Resolvers
+
+//typeDefs
+
